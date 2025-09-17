2f1e5237a7d6497060c3637b0a094d6a23759bac
Touricks <touricks@flagcamp.com>
2025-09-16 20:56:44 -0400

    Initial commit: AI Trip Planner backend project

---
+++ src/main/java/org/example/Planning/AiTripPlannerApplication.java
@@ -0,0 +1,13 @@
+package org.example.aitripplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiTripPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiTripPlannerApplication.class, args);
+    }
+
+}
